0d30f95b4b1c3db0870e521a4cfa45bab9956fa1
IsmoilovMax <maxmudjon2121@gmail.com>
2025-03-18 11:40:37 +0900

    feat: ConnectMongoDb an Express Port

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+const { PORT, MONGO_URL } = process.env
+
+mongoose
+	.connect(MONGO_URL as string, {})
+	.then(data => {
+		console.log('MongoDB Connected')
+		app.listen(PORT, () => {
+			console.info(`Server is running on http://localhost:${PORT}`)
+		})
+	})
+	.catch(err => console.log(`Error on connection MongoDb`, err))
